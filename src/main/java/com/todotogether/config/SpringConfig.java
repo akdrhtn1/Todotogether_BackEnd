cc8269cd0c34e0b063ed8c8360b40fdb5c0e2be3
akdrhtn1 <akdrhtn@kakao.com>
2021-12-31 01:25:36 +0900

    initial commit

---
+++ src/main/java/com/todotogether/config/SpringConfig.java
@@ -0,0 +1,8 @@
+package com.todotogether.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+}
---
+++ src/test/java/com/todotogether/TodotogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todotogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
